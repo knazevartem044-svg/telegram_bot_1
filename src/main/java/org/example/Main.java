1aaaa8b93d4c4021f68be2aec5aae1d53a5c3c5a
knazevartem044-svg <knazevartem044@gmail.com>
2025-10-06 20:43:59 +0500

    убрал лишнее из кода

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,16 +4,12 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class Main {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        Dotenv dotenv = Dotenv.configure().load();
 
         String token = System.getenv("TOKEN_BOT");
         if (token == null || token.isBlank()) {
             token = dotenv.get("TOKEN_BOT");
         }
-        if (token == null || token.isBlank()) {
-            throw new IllegalArgumentException(".env");
-        }
-
         TgBot bot = new TgBot(token);
         bot.start();
     }
